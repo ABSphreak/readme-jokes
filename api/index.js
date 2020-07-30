13c14a40e84b36c50921d2882ef8aaf88e1f2722
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-07-30 22:24:16 +0530

    feat: dynamic jokes

--- api/index.js
+++ api/index.js
@@ -1,92 +1,23 @@
+const jokes = require("../src/jokes.json");
+
+let index = Math.floor(Math.random() * 6 + 1);
+
+let question = jokes[index].question;
+let answer = jokes[index].answer;
+
 module.exports = async (req, res) => {
-  const { username } = req.query;
-
   res.setHeader("Content-Type", "image/svg+xml");
-
-  res.send(`<svg fill="none" viewBox="0 0 800 400" width="800" height="400" xmlns="http://www.w3.org/2000/svg">
-	<foreignObject width="100%" height="100%">
-		<div xmlns="http://www.w3.org/1999/xhtml">
-			<style>
-				@keyframes rotate {
-					0% {
-						transform: rotate(3deg);
-					}
-					100% {
-						transform: rotate(-3deg);
-					}
-				}
-				@keyframes gradientBackground {
-					0% {
-						background-position: 0% 50%;
-					}
-					50% {
-						background-position: 100% 50%;
-					}
-					100% {
-						background-position: 0% 50%;
-					}
-				}
-				@keyframes fadeIn {
-					0% {
-						opacity: 0;
-					}
-					66% {
-						opacity: 0;
-					}
-					100% {
-						opacity: 1;
-					}
-				}
-				.container {
-					font-family:
-						system-ui,
-						-apple-system,
-						'Segoe UI',
-						Roboto,
-						Helvetica,
-						Arial,
-						sans-serif,
-						'Apple Color Emoji',
-						'Segoe UI Emoji';
-					display: flex;
-					flex-direction: column;
-					align-items: center;
-					justify-content: center;
-					margin: 0;
-					width: 100%;
-					height: 400px;
-					background: linear-gradient(-45deg, #fc5c7d, #6a82fb, #05dfd7);
-					background-size: 600% 400%;
-					animation: gradientBackground 10s ease infinite;
-					border-radius: 10px;
-					color: white;
-					text-align: center;
-				}
-				h1 {
-					font-size: 50px;
-					line-height: 1.3;
-					letter-spacing: 5px;
-					text-transform: uppercase;
-					text-shadow:
-						0 1px 0 #efefef,
-						0 2px 0 #efefef,
-						0 3px 0 #efefef,
-						0 4px 0 #efefef,
-						0 12px 5px rgba(0, 0, 0, 0.1);
-					animation: rotate ease-in-out 1s infinite alternate;
-				}
-				p {
-					font-size: 20px;
-					text-shadow: 0 1px 0 #efefef;
-					animation: 5s ease 0s normal forwards 1 fadeIn;
-				}
-			</style>
-			<div class="container">
-        <h1>Vercel Test</h1>
-        <p>${username}</p>
-			</div>
-		</div>
-	</foreignObject>
-</svg>
+  res.send(`<svg width="495" height="120" viewBox="0 0 495 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<style>
+	.text { font: 600 16px  monospace; fill: #2F80ED }
+	.question { font: monospace; fill: #fca311 }
+	.answer { fill: #02c39a }
+	</style>
+	<rect x="0.5" y="0.5" width="494" height="99%" rx="4.5" fill="#5f0f40" stroke="#fb8b24"/>
+	<text x="25" y="35" class="text">
+		<tspan x="25" dy="18" class="question">Q. ${question}</tspan>
+		<tspan x="25" dy="18" class="answer">A. ${answer}</tspan>
+	</text>
+	</svg>
 `);
 };
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+function pickRandomJoke(jokes) {
+  
+}
