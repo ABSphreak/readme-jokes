f51d2bbd26affc96e6cf62a23ac2a7c2377c9a57
ABSphreak <absphreak@outlook.com>
2020-10-17 23:49:39 +0530

    🔧 fix themes

--- src/themes.js
+++ src/themes.js
@@ -1,61 +1,61 @@
-
 const theme = {
-	default: {
-					container_border: "2px solid #8ac926",
-					container_background: "#242423",
-					question_color: "#ffca3a",
-					answer_color: "#8ac926",
-					quote_color: "#ffca3a",
-					code_color: "#f72585";
-				},
-	gradient-blue: {
-					container_border: "1px solid #72e7ff",
-					container_background: "linear-gradient(26deg, rgba(4,26,48,1) 0%, rgba(33,93,142,1) 54%, rgba(0,212,255,1) 100%)",
-					question_color: "#B3B8BD",
-					answer_color: "#D6DEE6",
-					quote_color: "#D6DEE6",
-					code_color: "#f72585";
-				},
-	solid-blue: {
-					container_border: "1px solid #72e7ff",
-					container_background: "rgba(4,26,48,1)",
-					question_color: "#2289F0",
-					answer_color: "#63B1FF",
-					quote_color: "#63B1FF",
-					code_color: "#f72585";
-				},
-	halloween: {
-					container_border: "1px solid #DE2C2C",
-					container_background: "rgba(21,3,3,1)",
-					question_color: "#DE2C2C",
-					answer_color: "#ef9696",
-					quote_color: "#DE2C2C",
-					code_color: "#f72585";
-				},
-	watermelon: {
-					container_border: "1px solid #28d0dc",
-					container_background: "rgba(3,21,14,1)",
-					question_color: "#caf6e3",
-					answer_color: "#2cdd93",
-					quote_color: "#2cdd93",
-					code_color: "#f72585";
-				},
-	pink-int: {
-					container_border: "1px solid #FF7F9D",
-					container_background: "#CF4B6A",
-					question_color: "#FBC0FC",
-					answer_color: "#FEE0FF",
-					quote_color: "#FEE0FF",
-					code_color: "#ffffff";
-	},
-	day-sky: {
-					container_border: "1px solid #1777D6",
-					container_background: "#ABCDEF",
-					question_color: "#074685",
-					answer_color: "#FEE0FF",
-					quote_color: "#0C6FD1",
-					code_color: "#f72585";
-	},
+  default: {
+    container_border: '2px solid #8ac926',
+    container_background: '#242423',
+    question_color: '#ffca3a',
+    answer_color: '#8ac926',
+    quote_color: '#ffca3a',
+    code_color: '#f72585',
+  },
+  gradientBlue: {
+    container_border: '1px solid #72e7ff',
+    container_background:
+      'linear-gradient(26deg, rgba(4,26,48,1) 0%, rgba(33,93,142,1) 54%, rgba(0,212,255,1) 100%)',
+    question_color: '#B3B8BD',
+    answer_color: '#D6DEE6',
+    quote_color: '#D6DEE6',
+    code_color: '#f72585',
+  },
+  solidBlue: {
+    container_border: '1px solid #72e7ff',
+    container_background: 'rgba(4,26,48,1)',
+    question_color: '#2289F0',
+    answer_color: '#63B1FF',
+    quote_color: '#63B1FF',
+    code_color: '#f72585',
+  },
+  halloween: {
+    container_border: '1px solid #DE2C2C',
+    container_background: 'rgba(21,3,3,1)',
+    question_color: '#DE2C2C',
+    answer_color: '#ef9696',
+    quote_color: '#DE2C2C',
+    code_color: '#f72585',
+  },
+  watermelon: {
+    container_border: '1px solid #28d0dc',
+    container_background: 'rgba(3,21,14,1)',
+    question_color: '#caf6e3',
+    answer_color: '#2cdd93',
+    quote_color: '#2cdd93',
+    code_color: '#f72585',
+  },
+  pinkish: {
+    container_border: '1px solid #FF7F9D',
+    container_background: '#CF4B6A',
+    question_color: '#FBC0FC',
+    answer_color: '#FEE0FF',
+    quote_color: '#FEE0FF',
+    code_color: '#ffffff',
+  },
+  daySky: {
+    container_border: '1px solid #1777D6',
+    container_background: '#ABCDEF',
+    question_color: '#074685',
+    answer_color: '#FEE0FF',
+    quote_color: '#0C6FD1',
+    code_color: '#f72585',
+  },
 };
 
 module.exports(theme);
